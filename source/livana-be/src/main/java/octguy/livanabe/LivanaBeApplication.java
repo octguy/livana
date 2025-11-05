1b3504fb4d147fadbcb68fb3a4ff86d9b4c3e0d2
OctGuy <heyiamthinh1003@gmail.com>
2025-11-05 11:54:55 +0700

    feat: add initial Spring Boot application and configuration files

---
+++ source/livana-be/src/main/java/octguy/livanabe/LivanaBeApplication.java
@@ -0,0 +1,13 @@
+package octguy.livanabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivanaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivanaBeApplication.class, args);
+    }
+
+}
---
+++ source/livana-be/src/test/java/octguy/livanabe/LivanaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package octguy.livanabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivanaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
