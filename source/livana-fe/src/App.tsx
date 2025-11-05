e55cfe39d0620a2d8dcffd3a43a1096bc38fd04c
OctGuy <heyiamthinh1003@gmail.com>
2025-11-05 20:32:38 +0700

    feat: integrate Tailwind CSS and Radix UI components; add login and signup pages
    - Updated package.json to include new dependencies for Tailwind CSS, Radix UI, and form handling.
    - Refactored App component to implement routing for login and signup pages.
    - Created LogInPage and SignUpPage components with basic structure.
    - Implemented SignupForm component with form fields and validation placeholders.
    - Enhanced styling with Tailwind CSS and added utility classes for animations and gradients.
    - Configured Vite to resolve paths and integrate Tailwind CSS.
    - Added utility functions for class name management.
    - Established a design system with reusable UI components (Button, Card, Input, Label, Separator).
    - Updated TypeScript configuration for path aliases.

--- source/livana-fe/src/App.tsx
+++ source/livana-fe/src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
+import { Toaster } from "sonner";
+
 function App() {
   return (
     <>
-      <p>hello</p>
+      <Toaster richColors />
+      <BrowserRouter>
+        {/* public route */}
+        <Routes>
+          <Route path="/login" element={<LogInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+
+        {/* protected route */}
+      </BrowserRouter>
     </>
   );
 }
